545ccfa9bb506f058adec5b7b8f8afd0774e681a
Arki Valencia II <arvalencia@gbox.adnu.edu.ph>
2017-09-07 15:09:49 +0000

    wip

--- routes/botman.php
+++ routes/botman.php
@@ -3,9 +3,9 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('login {username} {password}', 'App\Http\Controllers\BotController@login');
-
 $botman->hears('help', 'App\Http\Controllers\BotController@help');
+
+$botman->hears('login {username} {password}', 'App\Http\Controllers\BotController@login');
 
 $botman->fallback(function($bot) {
     $bot->reply('Please login to use the bot.');
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,6 @@ Route::match(['get', 'post'], '/botman', 'BotManController@handle');
 
 Route::get('/botman/tinker', 'BotManController@tinker');
 
-Route::get('/test', 'TestController@test');
-
 Route::get('/privacy-policy', function(){
 	return view('privacy');
 });
